e28dd5f307d55566e3a74725279da4df5596005a
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-07-30 12:38:09 +0700

    feat: add content to not found page

--- src/app/routes/not-found-page.tsx
+++ src/app/routes/not-found-page.tsx
@@ -1,1 +1,16 @@
-export const NotFoundPage = () => <>Not Found</>;
+import { FormLayout } from "@/features/shared/components/layouts/form-layout";
+import { buttonVariants } from "@/features/shared/components/ui/button";
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+
+export const NotFoundPage = () => (
+	<FormLayout
+		title="😵‍💫 404"
+		subtitle="Oops, Looks like the page you're trying to access is not missing!"
+	>
+		<Link to="/" className={buttonVariants()}>
+			<ArrowLeftIcon className="size-5" />
+			Return to quiz selection
+		</Link>
+	</FormLayout>
+);
