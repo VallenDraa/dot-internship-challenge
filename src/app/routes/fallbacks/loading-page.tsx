dceeffcb5ca309aa4951aec3c4da35138829e169
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-07-31 00:00:48 +0700

    chore: remove console.log statement in MainLoadingFallback component

--- src/app/routes/fallbacks/loading-page.tsx
+++ src/app/routes/fallbacks/loading-page.tsx
@@ -1,7 +1,6 @@
 import { LoadingSpinner } from "../../../features/shared/components/ui/loading-spinner";
 
 export const MainLoadingFallback = () => {
-	console.log("Sds");
 	return (
 		<div className="flex h-screen flex-col items-center justify-center bg-black">
 			<LoadingSpinner />
